adbd9b680980b3fc496e39618450413e5e9bff1d
yangwestyyy21 <33038975+yangwestyyy21@users.noreply.github.com>
2022-02-24 22:54:54 -0800

    fsfsafd

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,6 +1,4 @@
 import static org.junit.Assert.*;
-
-
 import org.junit.*;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -12,12 +10,14 @@ public class MarkdownParseTest {
     @Test
     public void parse() throws IOException {
         ArrayList<String>[] list = new ArrayList[3];
-        list[0] = new ArrayList();
-        list[1] = new ArrayList();
-        list[2] = new ArrayList();
-        list[0].add("[`google.com]");
-        list[1].add("[a.com, a.com(()), example.com]");
-        list[2].add("[https://ucsd-cse15l-w22.github.io/]");
+        list[0] = new ArrayList<String>();
+        list[1] = new ArrayList<String>();
+        list[2] = new ArrayList<String>();
+        list[0].add("`google.com");
+        list[1].add("a.com");
+        list[1].add("a.com(())");
+        list[1].add("example.com");
+        list[2].add("https://ucsd-cse15l-w22.github.io/");
 
         for (int i = 0; i < 3; i++) {
             String file = load("snippet" + (i + 1) + ".md");
